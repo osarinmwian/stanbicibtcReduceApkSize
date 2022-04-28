3bca5e483b507c3844bcc644cefb6764ef7fac3c
Samuel <samuel.fayomi@stanbicibtc.com>
2022-04-28 08:36:56 +0100

    fix: splashscreen

---
+++ src/mybank/screens/lifestyle/lifestyleLandingPage.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+import { loginBackground } from "@/mybank/assets/image";
+import BottomTabLandingPageTopBar from "@/mybank/components/TopBar";
+import { MyBankNavigationProps } from "@/mybank/navigation/types";
+import { ImageIconPackType } from "@/shared/assets/icons";
+import { Box } from "@/shared/components/Layout";
+
+import LifestylePicker from "./components/lifestylePicker";
+
+const LifestyleLandingPage = ({
+  navigation,
+}: MyBankNavigationProps<"LifestyleLandinPage">) => {
+  const lifestyleData = [
+    {
+      destination: "FlightIndex",
+      heading: "Flights",
+      iconName: "plane",
+      id: "1",
+      subHeading: "Book flights easily",
+    },
+    {
+      destination: "FlightIndex",
+      heading: "Find and make hotel reservations",
+      iconName: "bed",
+      id: "2",
+      subHeading: "Book flights easily",
+    },
+    {
+      destination: "FlightIndex",
+      heading: "Centili fusion",
+      iconName: "centili",
+      id: "3",
+      subHeading: "Digital lifestyle experience",
+    },
+  ];
+  return (
+    <>
+      <BottomTabLandingPageTopBar
+        heading="LIFESTYLE"
+        imageName={loginBackground}
+        landingPage
+      />
+      <Box
+        backgroundColor="whiteColor"
+        borderTopEndRadius="md"
+        borderTopStartRadius="md"
+        flex={1}
+        marginTop="rmd"
+        padding="md"
+      >
+        {lifestyleData.map((item) => (
+          <LifestylePicker
+            destination={() =>
+              navigation.navigate("Lifestyle", { screen: item.destination })
+            }
+            heading={item.heading}
+            key={item.id}
+            name={item.iconName as ImageIconPackType}
+            subHeading={item.subHeading}
+          />
+        ))}
+      </Box>
+    </>
+  );
+};
+
+export default LifestyleLandingPage;
