38b028b341789857f55593875bac04a5f0862637
strevo199 <mgbeojikwestephen@gmail.com>
2022-08-25 09:53:37 +0100

    feat: hotel-dev

--- src/mybank/components/Cards/EligibilityCard.tsx
+++ src/mybank/components/Cards/EligibilityCard.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import React, { VFC } from "react";
 
 import { PrimaryButton } from "@/shared/components/Buttons";
@@ -7,54 +8,54 @@ import { Text } from "@/shared/components/Typography";
 const EligibilityCard: VFC = () => {
   const navigation = useNavigation();
   return (
-  <Card backgroundColor="loanCard" padding="md">
-    <Box>
-      <Box alignItems="flex-start" mb="md">
-        <Box
-          borderColor="mutualFundsContainer"
-          borderRadius="xs"
-          borderWidth={1}
-          paddingHorizontal="sm"
-          paddingVertical="xs"
-        >
-          <Text color="mutualFundsContainer" fontVariant="caption">
-            Eligibility
+    <Card backgroundColor="loanCard" padding="md">
+      <Box>
+        <Box alignItems="flex-start" mb="md">
+          <Box
+            borderColor="mutualFundsContainer"
+            borderRadius="xs"
+            borderWidth={1}
+            paddingHorizontal="sm"
+            paddingVertical="xs"
+          >
+            <Text color="mutualFundsContainer" fontVariant="caption">
+              Eligibility
+            </Text>
+          </Box>
+        </Box>
+        <Box>
+          <Text>EZ Cash Loan</Text>
+          <Text fontVariant="h4" fontWeight="bold">
+            N4,000,000
           </Text>
         </Box>
-      </Box>
-      <Box>
-        <Text>EZ Cash Loan</Text>
-        <Text fontVariant="h4" fontWeight="bold">
-          N4,000,000
+        <Text color="textColor2" my="md">
+          Click on the button below to proceed
         </Text>
+        <Box>
+          <PrimaryButton
+            containerProps={{ flex: 1 }}
+            label="PROCEED"
+            labelProps={{ color: "whiteColor", fontVariant: "caption" }}
+            paddingVertical="sm"
+          />
+        </Box>
+        <Box mt="sm">
+          <PrimaryButton
+            containerProps={{ flex: 1 }}
+            label="SEE ALL LOAN OFFERS"
+            labelProps={{
+              color: "primaryColor",
+              fontVariant: "caption",
+              fontWeight: "bold",
+            }}
+            onPress={() => navigation.navigate("LoansAndOffers")}
+            variant="transparent"
+          />
+        </Box>
       </Box>
-      <Text color="textColor2" my="md">
-        Click on the button below to proceed
-      </Text>
-      <Box>
-        <PrimaryButton
-          containerProps={{ flex: 1 }}
-          label="PROCEED"
-          labelProps={{ color: "whiteColor", fontVariant: "caption" }}
-          paddingVertical="sm"
-        />
-      </Box>
-      <Box mt="sm">
-        <PrimaryButton
-          containerProps={{ flex: 1 }}
-          label="SEE ALL LOAN OFFERS"
-          labelProps={{
-            color: "primaryColor",
-            fontVariant: "caption",
-            fontWeight: "bold",
-          }}
-          variant="transparent"
-          onPress={() => navigation.navigate("LoansAndOffers")}
-        />
-      </Box>
-    </Box>
-  </Card>
-  )
+    </Card>
+  );
 };
 
 export { EligibilityCard };
